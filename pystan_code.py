a4b2b8628b35d79a1c9240bc46995004162b8a62
Brandon Onyejekwe <brandononyejekwe@Brandons-MacBook-Pro.local>
2025-02-11 18:46:28 -0500

    Develop separate and combined stan bayes models

--- pystan_code.py
+++ pystan_code.py
@@ -1,3 +1,4 @@
+
 import stan
 import numpy as np
 import pandas as pd
@@ -9,47 +10,37 @@ with open("marathon.stan", "r") as f:
 train_df = pd.read_csv("train.csv")#[:50]
 print(len(train_df))
 
-feats = ["total_pace", "curr_pace", "prop"]
-schools_data = {"feats": train_df[feats].values, "finish": train_df["finish"].values, "N": len(train_df), "K": len(feats)}
-
-
 def single_model(stan_code, data_dict, outpath="stan_results/ps_result.csv"):
+    """Fit a single stan model, incorporating all dists"""
     posterior = stan.build(stan_code, data=data_dict, random_seed=2)
     fit = posterior.sample(num_chains=4, num_samples=1000)
     df = fit.to_frame()
     df.to_csv(outpath)
     return # posterior, fit
 
-def separate_models(stan_code, data):
-    feats = ["total_pace", "curr_pace"]
-
+def separate_models(stan_code, data, feats_sep, outdir="stan_results"):
+    """Fit an inividual stan model for each dist"""
     marks = ["5K", "10K", "15K", "20K", "25K", "30K", "35K", "40K"]
     for mk in marks:
         print("loading: ", mk)
         df = data[data["dist"] == mk]
-        schools_data = {"feats": df[feats].values, "finish": df["finish"].values, "N": len(df), "K": len(feats)}
-        single_model(schools_code, schools_data, outpath=f"stan_results/ps_result{mk}.csv")
+        schools_data = {"feats": df[feats_sep].values, "finish": df["finish"].values, "N": len(df), "K": len(feats_sep)}
+        single_model(schools_code, schools_data, outpath=f"{outdir}/ps_result{mk}.csv")
     return
 
 print('start')
-single_model(schools_code, schools_data)
-separate_models(schools_code, train_df)
+outdir = "stan_results/bayes1"
+feats = ["total_pace", "prop"]
+schools_data = {"feats": train_df[feats].values, "finish": train_df["finish"].values, "N": len(train_df), "K": len(feats)}
+single_model(stan_code=schools_code, data_dict=schools_data, outpath=f"{outdir}/ps_result.csv")
+separate_models(stan_code=schools_code, data=train_df, feats_sep=feats[:-1], outdir=outdir)
 
-# posterior, fit = single_model(schools_code, schools_data)
+outdir = "stan_results/bayes2"
+feats = ["total_pace", "curr_pace", "prop"]
+schools_data = {"feats": train_df[feats].values, "finish": train_df["finish"].values, "N": len(train_df), "K": len(feats)}
+single_model(stan_code=schools_code, data_dict=schools_data, outpath=f"{outdir}/ps_result.csv")
+separate_models(stan_code=schools_code, data=train_df, feats_sep=feats[:-1], outdir=outdir)
 print('end')
-
-# import arviz as az
-# idata = az.from_pystan(posterior=fit, posterior_model=posterior)
-
-# print(idata)
-# print(type(idata))
-# print(type(fit))
-# print(type(posterior))
-# print(idata.posterior)
-# print('a')
-# print(idata.sample_stats)
-# print(';done')
-# idata.to_netcdf('save_idata.nc')
 
 
 # if __name__ == "__main__":
