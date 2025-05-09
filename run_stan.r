0b720f6ee08af50fd0e6bd5566d5aa6e43f29e5a
Brandon Onyejekwe <brandononyejekwe@Brandons-MacBook-Pro.local>
2025-05-09 17:05:19 -0400

    Rename r file, remove absolute paths in r file

--- run_stan.r
+++ run_stan.r
@@ -3,7 +3,7 @@
 # install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies = TRUE)
 # example(stan_model, package = "rstan", run.dontrun = TRUE)
 library("rstan") # observe startup messages
-train_data <- read.csv("/Users/brandononyejekwe/Documents/Personal_Projects/Marathon_Predictor/processed_data/train.csv")
+train_data <- read.csv("processed_data/train.csv")
 features <- c("total_pace", "curr_pace", "prop") #, "propxcurr", "male", "age", "malexage")
 
 schools_dat <- list(N = nrow(train_data),
@@ -12,10 +12,10 @@ schools_dat <- list(N = nrow(train_data),
                     propleft = train_data$propleft,
                     finish = train_data$finish)
 
-fit <- stan(file = '/Users/brandononyejekwe/Documents/Personal_Projects/Marathon_Predictor/marathon.stan', data = schools_dat)
+fit <- stan(file = 'marathon.stan', data = schools_dat)
 d <- as.data.frame(fit)
-write.csv(d,"/Users/brandononyejekwe/Documents/Personal_Projects/Marathon_Predictor/stan_results/rs_result2dw.csv", row.names = TRUE)
-# getwd()
+write.csv(d,"stan_results/rs_result2dw.csv", row.names = TRUE)
+getwd()
 
 
 
